8782c167af1d5e86c7630dab32ab8dc07c4aebef
ivanlhz <ivanlhz@gmail.com>
2020-04-05 14:07:03 +0100

    Add PinInput component behavior

--- src/components/PinInput/PinInput.js
+++ src/components/PinInput/PinInput.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './styles.scss';
+import PropTypes from 'prop-types';
 
-const PinInput = () => {
-  return <h2>Pin Input</h2>;
+const PinInput = ({ pin, value }) => {
+  const [output, setOutput] = useState(() => '');
+
+  useEffect(() => {
+    valueHandler();
+  }, [value]);
+
+  const valueHandler = () => {
+    if (value.length === 4) {
+      if (pin !== value) {
+        setOutput('ERROR');
+      } else {
+        setOutput('OK');
+      }
+    } else {
+      setOutput(formatOuput());
+    }
+  };
+
+  const formatOuput = () => {
+    if (value.length === 1) {
+      return value;
+    } else if (value.length > 0) {
+      let toReturn = new Array(value.length - 1).fill('*');
+      return toReturn.join('') + value[value.length - 1];
+    }
+  };
+
+  return <div className="input-text">{output}</div>;
+};
+
+PinInput.propTypes = {
+  pin: PropTypes.string.isRequired,
+  value: PropTypes.string,
 };
 
 export default PinInput;
