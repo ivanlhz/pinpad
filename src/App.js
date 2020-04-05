78137ade42b66d89b65c365f669066387ba30a51
ivanlhz <ivanlhz@gmail.com>
2020-04-05 19:09:44 +0100

    Add App.js behavior

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { PinPad, PinInput } from './components';
 
 function App() {
+  const getPin = () => Math.floor(Math.random() * 10000 + 1).toString();
+  const [number, setNumber] = useState('');
+  const [pin] = useState(() => getPin());
+
+  const pressHandler = (value) => {
+    if (number.length === 4) {
+      setNumber(value.toString());
+    } else {
+      setNumber(number + value.toString());
+    }
+  };
+
   return (
     <div className="App">
-      <PinPad />
-      <PinInput />
+      <h2>Unlock with your pin code</h2>
+      <PinInput pin={pin} code={number} />
+      <PinPad onNumberPress={pressHandler} />
     </div>
   );
 }
