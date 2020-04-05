c63e3208b083c8ef63efa87c59d97c1232f7bf32
ivanlhz <ivanlhz@gmail.com>
2020-04-05 14:06:30 +0100

    Fix PinInput test

--- src/components/PinInput/PinInput.test.js
+++ src/components/PinInput/PinInput.test.js
@@ -3,43 +3,26 @@ import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import PinInput from './PinInput';
 
-test('Input value should be *. But the last character should be a number', () => {
-  const { getByRole } = render(<PinInput pin={'1111'} />);
-  const inputElement = getByRole('code');
-  userEvent.type('124');
-  expect(inputElement).toHaveTextContent('**4');
+test('When the input value length is 1, it value should be the number', () => {
+  const { getByText } = render(<PinInput pin={'1111'} value={'1'} />);
+  expect(getByText('1')).toBeInTheDocument();
 });
 
-test('When the input value length is 1, it value should be the number', () => {
-  const { getByRole } = render(<PinInput pin={'1111'} />);
-  const inputElement = getByRole('code');
-  userEvent.type('1');
-  expect(inputElement).toHaveTextContent('1');
+test('Input value should be *. But the last character should be a number', () => {
+  const { getByText } = render(<PinInput pin={'1111'} value={'124'} />);
+  const inputElement = getByText(/\*\*4/i);
+
+  expect(inputElement).toBeInTheDocument();
 });
 
 test('When the input value is the right pin code display OK', () => {
-  const { getByRole } = render(<PinInput pin={'1111'} />);
-  const inputElement = getByRole('code');
-  userEvent.type('1111');
-  expect(inputElement).toHaveTextContent('OK');
+  const { getByText } = render(<PinInput pin={'1111'} value={'1111'} />);
+  const inputElement = getByText(/ok/i);
+  expect(inputElement).toBeInTheDocument();
 });
 
 test('When the input value is the wrong pin code display ERROR', () => {
-  const mockFunc = jest.fn();
-  const { getByRole } = render(<PinInput pin={'1111'} onResetPin={mockFunc} />);
-  const inputElement = getByRole('code');
-  userEvent.type('2222');
-  expect(inputElement).toHaveTextContent('ERROR');
-  expect(mockFunc).toHaveBeenCalledTimes(1);
+  const { getByText } = render(<PinInput pin={'1111'} value={'1234'} />);
+  const inputElement = getByText(/error/i);
+  expect(inputElement).toBeInTheDocument();
 });
-
-// test('When the pin code is wrong 3 times display LOCKED', () => {
-//   const mockFunc = jest.fn();
-//   const { getByRole } = render(<PinInput pin={'1111'} onResetPin={mockFunc} />);
-//   const inputElement = getByRole('code');
-//   userEvent.type('2222');
-//   userEvent.type('3333');
-//   userEvent.type('4444');
-//   expect(inputElement).toHaveTextContent('LOCKED');
-//   expect(mockFunc).toHaveBeenCalledTimes(1);
-// });
