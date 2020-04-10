a424153738fa05e1ac8cf3add9d741a82745515f
ivanlhz <ivanlhz@gmail.com>
2020-04-10 08:36:56 +0100

    Added PinPad stories

---
+++ src/stories/1-PinPad.stories.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {withKnobs, boolean} from '@storybook/addon-knobs'
+import {action} from '@storybook/addon-actions'
+import {PinPad} from '../components'
+
+
+
+export default {
+  title: 'PinPad',
+  decorators: [withKnobs]
+}
+
+export const defaultState = () => {
+  return <PinPad  />
+}
+
+export const disabled = () => {
+  const disabled = boolean('Disabled', true)
+  return <PinPad disabled={disabled} />
+}
+
+export const clickEvent = () => {
+  const disabled = boolean('Disabled', false)
+  
+  return <PinPad disabled={disabled} onNumberPress={action('pinPad-click')} />
+}
