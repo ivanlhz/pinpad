eba72782e9e2b61f8d36c0533f2a1b9ba891bae6
ivanlhz <ivanlhz@gmail.com>
2020-04-10 08:37:58 +0100

    Fix format

--- src/stories/0-PinInput.stories.js
+++ src/stories/0-PinInput.stories.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { withKnobs, text } from '@storybook/addon-knobs';
-import {PinInput} from '../components'
+import { PinInput } from '../components';
 
 export default {
   title: 'PinInput',
-  decorators: [withKnobs]
-}
+  decorators: [withKnobs],
+};
 
 export const defaultState = () => {
-  const pin = text("Pin", '1111');
-  const inputCode = text("InputCode", '');
- 
+  const pin = text('Pin', '1111');
+  const inputCode = text('InputCode', '');
+
   return <PinInput pin={pin} code={inputCode} />;
 };
 
 export const success = () => {
-  const pin = text("Pin", '1111');
-  const inputCode = text("InputCode", '1111');
- 
+  const pin = text('Pin', '1111');
+  const inputCode = text('InputCode', '1111');
+
   return <PinInput pin={pin} code={inputCode} />;
 };
 export const error = () => {
-  const pin = text("Pin", '1111');
-  const inputCode = text("InputCode", '1112');
- 
+  const pin = text('Pin', '1111');
+  const inputCode = text('InputCode', '1112');
+
   return <PinInput pin={pin} code={inputCode} />;
 };
--- src/stories/1-PinPad.stories.js
+++ src/stories/1-PinPad.stories.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import {withKnobs, boolean} from '@storybook/addon-knobs'
-import {action} from '@storybook/addon-actions'
-import {PinPad} from '../components'
-
-
+import React from 'react';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import { PinPad } from '../components';
 
 export default {
   title: 'PinPad',
-  decorators: [withKnobs]
-}
+  decorators: [withKnobs],
+};
 
 export const defaultState = () => {
-  return <PinPad  />
-}
+  return <PinPad />;
+};
 
 export const disabled = () => {
-  const disabled = boolean('Disabled', true)
-  return <PinPad disabled={disabled} />
-}
+  const disabled = boolean('Disabled', true);
+  return <PinPad disabled={disabled} />;
+};
 
 export const clickEvent = () => {
-  const disabled = boolean('Disabled', false)
-  
-  return <PinPad disabled={disabled} onNumberPress={action('pinPad-click')} />
-}
+  const disabled = boolean('Disabled', false);
+
+  return <PinPad disabled={disabled} onNumberPress={action('pinPad-click')} />;
+};
