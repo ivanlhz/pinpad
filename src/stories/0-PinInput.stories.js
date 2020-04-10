83ecf1395012e567054c9c1ba3044c17e9ca4075
ivanlhz <ivanlhz@gmail.com>
2020-04-10 08:24:57 +0100

    Add storybook, PinInput storie and add knobs addon

---
+++ src/stories/0-PinInput.stories.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { withKnobs, text } from '@storybook/addon-knobs';
+import {PinInput} from '../components'
+
+export default {
+  title: 'PinInput',
+  decorators: [withKnobs]
+}
+
+export const defaultState = () => {
+  const pin = text("Pin", '1111');
+  const inputCode = text("InputCode", '');
+ 
+  return <PinInput pin={pin} code={inputCode} />;
+};
+
+export const success = () => {
+  const pin = text("Pin", '1111');
+  const inputCode = text("InputCode", '1111');
+ 
+  return <PinInput pin={pin} code={inputCode} />;
+};
+export const error = () => {
+  const pin = text("Pin", '1111');
+  const inputCode = text("InputCode", '1112');
+ 
+  return <PinInput pin={pin} code={inputCode} />;
+};
