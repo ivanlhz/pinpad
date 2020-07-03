ebeb4cf6d40bd3deabc059a545e5dbf289b2e57e
ivanlhz <ivanlhz@gmail.com>
2020-07-03 16:55:20 +0100

    CYpress tests

--- cypress/e2e/pinpad.js
+++ cypress/e2e/pinpad.js
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 describe('Pin pad bheavior test', () => {
   it('show and Hide pincode', () => {
     cy.visit('/')
@@ -10,24 +11,7 @@ describe('Pin pad bheavior test', () => {
   });
   it('add number into the pad and shows error', () => {
     cy.get('.pin-pad > :nth-child(3)')
-      .click()
-      .get('.output-text')
-      .contains('7')
-      .get('.pin-pad > :nth-child(3)')
-      .click()
-      .get('.output-text')
-      .contains('*7')
-      .get('.pin-pad > :nth-child(3)')
-      .click()
-      .get('.pin-pad > :nth-child(3)')
-      .click()
-      .get('.output-text')
-      .contains('ERROR')
-  });
-  it('error 3 times disable pad', () => {
-    for(let i = 0; i <=3 ; i ++) {
-      cy.get('.pin-pad > :nth-child(3)')
-        .click()
+        .click({force:true})
         .get('.pin-pad > :nth-child(3)')
         .click()
         .get('.pin-pad > :nth-child(3)')
@@ -36,19 +20,60 @@ describe('Pin pad bheavior test', () => {
         .click()
         .get('.output-text')
         .contains('ERROR')
-    }
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.get('.pin-pad > :nth-child(3)')
-      .should('have.class','disabled')
-      .wait(30001).then( () => {
+  });
+  it('error 3 times disable pad', () => {
+    cy.visit('/')
+      .get('.pin-pad > :nth-child(3)')
+      .click({force:true})
+      .get('.pin-pad > :nth-child(3)')
+      .click()
+      .get('.pin-pad > :nth-child(3)')
+      .click()
+      .get('.pin-pad > :nth-child(3)')
+      .click()
+      .get('.pin-pad > :nth-child(3)')
+      .click()
+      .get('.output-text')
+      .contains('ERROR')
+      .wait(3001).then(() => {
         cy.get('.pin-pad > :nth-child(3)')
-        .should('not.have.class','disabled')
+          .click({force:true})
+          .get('.pin-pad > :nth-child(3)')
+          .click()
+          .get('.pin-pad > :nth-child(3)')
+          .click()
+          .get('.pin-pad > :nth-child(3)')
+          .click()
+          .get('.output-text')
+          .contains('ERROR')
+          .wait(3001).then(() => {
+            cy.get('.pin-pad > :nth-child(3)')
+            .click({force:true})
+              .get('.pin-pad > :nth-child(3)')
+              .click()
+              .get('.pin-pad > :nth-child(3)')
+              .click()
+              .get('.pin-pad > :nth-child(3)')
+              .click({force: true})
+              .get('.output-text')
+              .contains('LOCKED')
+              .get('.pin-pad > :nth-child(3)')
+              .should('have.class','disabled')
+              .wait(30001).then( () => {
+                cy.get('.pin-pad > :nth-child(3)')
+                .should('not.have.class','disabled')
+              })
+          })
       })
   });
   it('get the pin value, type it and check if OK message is displayed',() => {
-    cy.get('#showPin')
-      .get('.actions')
-      .contains('Hide')
+    cy.visit('/')
+    .get('.actions')
+    .contains('Show')
+    .get('#showPin')
+    .click()
+    .get('.actions')
+    .contains('Hide')
       .get('.actions > span').invoke('text').then( (text) => {
         const _text = text.trim()
         for(let i = 0; i < _text.length ; i++){
