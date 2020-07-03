f4088b4dec9cfb3a5f2eb4a1a09655d5c2d03362
ivanlhz <ivanlhz@gmail.com>
2020-07-03 14:20:17 +0100

    FIx PinInput tests

--- src/components/PinInput/PinInput.test.tsx
+++ src/components/PinInput/PinInput.test.tsx
@@ -3,12 +3,12 @@ import { render } from '@testing-library/react';
 import PinInput from './PinInput';
 
 test('When the input value length is 1, it value should be the number', () => {
-  const { getByText } = render(<PinInput pin={'1111'} userInputCode={'1'} />);
+  const { getByText } = render(<PinInput formState={'notFullFilled'} userInputCode={'1'} />);
   expect(getByText('1')).toBeInTheDocument();
 });
 
 test('Input value should be *. But the last character should be a number', () => {
-  const { getByText } = render(<PinInput pin={'1111'} userInputCode={'124'} />);
+  const { getByText } = render(<PinInput formState={'notFullFilled'} userInputCode={'124'} />);
   const inputElement = getByText(/\*\*/i);
   const span = getByText(/4/i);
   expect(inputElement).toBeInTheDocument();
@@ -16,14 +16,20 @@ test('Input value should be *. But the last character should be a number', () =>
 });
 
 test('When the input value is the right pin code display OK', () => {
-  const { getByText } = render(<PinInput pin={'1111'} userInputCode={'1111'} />);
+  const { getByText } = render(<PinInput formState={'success'} userInputCode={'1111'} />);
   const inputElement = getByText(/ok/i);
   expect(inputElement).toBeInTheDocument();
 });
 
 test('When the input value is the wrong pin code display ERROR', () => {
-  const { getByText } = render(<PinInput pin={'1111'} userInputCode={'1234'} />);
+  const { getByText } = render(<PinInput formState={'error'} userInputCode={'1234'} />);
   const inputElement = getByText(/error/i);
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('When the input value is the wrong pin code display LOCKED', () => {
+  const { getByText } = render(<PinInput formState={'locked'} userInputCode={'1234'} />);
+  const inputElement = getByText(/locked/i);
   expect(inputElement).toBeInTheDocument();
 });
 
